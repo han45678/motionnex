38be3e457ca38f854eef0773b0eef799f53d2e2c
han45678 <han0910812144@gmail.com>
2026-08-03 13:18:01 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Initialize Hero Swiper
+  // const heroSwiper = new Swiper('.hero-swiper', {
+    // Optional parameters
+    // loop: true,
+    // effect: 'fade',
+    // autoplay: {
+    //   delay: 5000,
+    //   disableOnInteraction: false,
+    // },
+
+    // If we need pagination
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     clickable: true,
+  //   },
+  // });
+
+  // Other JavaScript code for the site can go here.
+
+});
